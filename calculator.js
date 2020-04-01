39927e9f00222e57842c2d70826bd248fb87c893
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-03-31 21:40:44 -0700

    add - pratice js for basic cal

--- calculator.js
+++ calculator.js
@@ -1,5 +1,24 @@
-const exampleAdditionInput = {
-  num1: 3,
-  num2: 5,
-  operation: 'add',
-}
+// This line "loads" the prompt package and puts it into a variable we can use called prompt
+// Lines like this (that use require()) typically go on the top of the file
+const prompt = require("prompt");
+
+const calculateUserInput = function(error, promptInput) {
+    console.log(
+        "This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input",
+        promptInput
+    );
+
+    // Questions to ask and answer:
+    // What is promptInput?
+    // What data type? What does it hold? What does it represent?
+    // How do we read values from it? What syntax?
+    // How can we use it?
+    // Can we call our existing functions now, inside of this function?
+};
+
+//start the prompt
+prompt.start();
+
+//collect two numbers (num1 and num2) and an operation
+//then call the function `calculator` with the user input
+prompt.get(["num1", "num2", "operation"], calculateUserInput);
---
+++ pratice.js
@@ -0,0 +1,60 @@
+let num1 = 10;
+let num2 = 5;
+let operators = "*";
+
+function addNums(num1, num2) {
+    return num1 + num2;
+}
+
+// console.log(addNums(num1, num2));
+
+function subNums(num1, num2) {
+    return num1 - num2;
+}
+
+// console.log(subNums(num1, num2));
+
+function divNums(num1, num2) {
+    return num1 / num2;
+}
+
+// console.log(divNums(num1, num2));
+
+function mutliNums(num1, num2) {
+    return num1 * num2;
+}
+
+// console.log(mutliNums(num1, num2));
+
+function calcInput(num1, num2, operators) {
+    if (
+        operators === "a" ||
+        operators === "add" ||
+        operators === "addition" ||
+        operators === "+"
+    ) {
+        return addNums(num1, num2);
+    } else if (
+        operators === "s" ||
+        operators === "sub" ||
+        operators === "subtract" ||
+        operators === "-"
+    ) {
+        return subNums(num1, num2);
+    } else if (
+        operators === "d" ||
+        operators === "div" ||
+        operators === "divide" ||
+        operators === "/"
+    ) {
+        return divNums(num1, num2);
+    } else if (
+        operators === "m" ||
+        operators === "mutli" ||
+        operators === "mutliple" ||
+        operators === "*"
+    ) {
+        return mutliNums(num1, num2);
+    }
+}
+console.log(calcInput(num1, num2, operators));
